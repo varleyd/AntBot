362754daa9a2002d9f13941a9eafbdcd96b87a82
Dominic Varley <github@dominicvarley.com>
2026-01-18 21:33:20 +0000

    Update antbot-test.ts

--- antbot-test.ts
+++ antbot-test.ts
@@ -1,19 +1,23 @@
 /**
- * AntBot - Minimal Bluetooth Test
- * Use this to verify Bluetooth connection works
+ * AntBot - Bluetooth Test with all services
  */
 
 // Show "1" - starting up
 basic.showNumber(1)
-basic.pause(500)
+basic.pause(300)
 
-// Show "2" - starting Bluetooth
+// Show "2" - starting Bluetooth services
 basic.showNumber(2)
+
+// Start multiple Bluetooth services that the app might need
 bluetooth.startUartService()
+bluetooth.startLEDService()
+bluetooth.startButtonService()
+bluetooth.startIOPinService()
 
 // Show "3" - Bluetooth started successfully
 basic.showNumber(3)
-basic.pause(500)
+basic.pause(300)
 
 // Show heart - ready for connection
 basic.showIcon(IconNames.Heart)
@@ -23,11 +27,10 @@ bluetooth.onBluetoothConnected(function () {
 })
 
 bluetooth.onBluetoothDisconnected(function () {
-    basic.showIcon(IconNames.No)
+    basic.showIcon(IconNames.Heart)
 })
 
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
-    // Flash to show data received
     basic.showIcon(IconNames.Diamond)
     basic.pause(200)
     basic.showIcon(IconNames.Yes)
